15d8e33442a583fdfa2f31446ae2c7080fca322a
victorFernandez173 <victor.fer.gor@proton.me>
2023-10-15 16:23:46 +0200

    Formulario de login montado, faltan estilos y refactorizaciones

--- app/View/Components/Layouts/Basic.php
+++ app/View/Components/Layouts/Basic.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Guest extends Component
+class Basic extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class Guest extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.layouts.guest');
+        return view('components.layouts.basic');
     }
 }
--- resources/views/components/layouts/basic.blade.php
+++ resources/views/components/layouts/basic.blade.php
@@ -27,17 +27,8 @@
     {{-- Scripts --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-sans text-gray-900 antialiased">
-<div class="h-[100vh] bg-cover bg-posters-fondo">
-    <div>
-        <a href="/">
-            <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-        </a>
-    </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
-    </div>
-</div>
+<body>
+{{ $slot }}
 </body>
 </html>
