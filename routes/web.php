e274f31c2054bd0aee13fe0018505988190600de
victor.fernandez173 <victor.fer.gor@proton.me>
2023-10-18 18:39:33 +0200

    TODO

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-// TODO terminar login:  'recuerdame' checkbox como funciona?;  vistas olvidaste contraseña y registrarse; validación por email
+// TODO terminar login:  'recuerdame' checkbox como funciona?;  vistas olvidaste contraseña y registrarse; validación por email; y validación del formulario; livewire
 // TODO generar inserts sin procedures
 // TODO carResetPassword en authenticatedSessionController estudiar
 
